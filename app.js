0324f9a18c17e011dd737d9e358f865e945226bf
rajesh-ndv <nidadhavolu.rajesh@sap.com>
2023-03-17 14:55:13 +0530

    Added Octokit Connection
    Added Octokit Connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require('./db/connection');
+const { Octokit, App } = require("octokit");
 const env = require('dotenv').config({path: __dirname + '/.env'});
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,24 @@ if(!env || !env.parsed || !env.parsed.MONGODB_URI){
 }else {
   connectDB(env);
 }
+
+const octokit = new Octokit({
+  auth: env.parsed.GITHUB_TOKEN
+})
+
+octokit.request('GET /repos/{owner}/{repo}/deployments', {
+  owner: 'rajesh-ndv',
+  repo: 'authenticationMS',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+}).then(function success(oData){
+  console.log(JSON.stringify(oData));
+}).catch(function error(oError){
+  console.log(oError);
+})
+
+
  
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
